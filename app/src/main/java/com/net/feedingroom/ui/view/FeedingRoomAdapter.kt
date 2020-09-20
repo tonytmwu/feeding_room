d2ac545d073ed1ccdff609ac8f14ba6197b86962
Tony <tonywu@taiwanmobile.com>
2020-09-20 15:08:54 +0800

    create FeedingRoomAdapter.kt

---
+++ app/src/main/java/com/net/feedingroom/ui/view/FeedingRoomAdapter.kt
@@ -0,0 +1,44 @@
+package com.net.feedingroom.ui.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.*
+import coil.load
+import com.net.feedingroom.databinding.ViewFeedingRoomBinding
+import com.net.feedingroom.model.FeedingRoom
+
+class FeedingRoomAdapter: ListAdapter<FeedingRoom, FeedingRoomAdapter.ViewHolder>(diffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedingRoomAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val vb = ViewFeedingRoomBinding.inflate(inflater, parent, false)
+        return ViewHolder(vb)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(private val vb: ViewFeedingRoomBinding): RecyclerView.ViewHolder(vb.root) {
+        fun bind(data: FeedingRoom) {
+            vb.tvTitle.text = data.name
+            vb.tvPhoneNumber.text = data.tel
+            vb.tvAddress.text = data.address
+            data.photo?.split(" ")?.takeIf { it.isNotEmpty() }?.let { photos ->
+                val imgBaseUrl = "https://storage.googleapis.com/extreme-signer-123109.appspot.com/feedingroom/"
+                vb.ivPhoto.load(imgBaseUrl + photos[0])
+            }
+        }
+    }
+
+    companion object {
+        val diffCallback = object: DiffUtil.ItemCallback<FeedingRoom>() {
+            override fun areItemsTheSame(oldItem: FeedingRoom, newItem: FeedingRoom): Boolean {
+                return oldItem.number == newItem.number
+            }
+
+            override fun areContentsTheSame(oldItem: FeedingRoom, newItem: FeedingRoom): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
