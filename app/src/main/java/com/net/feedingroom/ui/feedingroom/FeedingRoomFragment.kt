39bd11abfbdec02274afa5481ea6d49790732201
Tony <tonywu@taiwanmobile.com>
2020-11-05 15:02:33 +0800

    enter search icon, auto hide keyboard

--- app/src/main/java/com/net/feedingroom/ui/feedingroom/FeedingRoomFragment.kt
+++ app/src/main/java/com/net/feedingroom/ui/feedingroom/FeedingRoomFragment.kt
@@ -1,10 +1,13 @@
 package com.net.feedingroom.ui.feedingroom
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.view.inputmethod.InputMethodManager.HIDE_IMPLICIT_ONLY
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -48,11 +51,17 @@ class FeedingRoomFragment : Fragment() {
             when(actionId) {
                 EditorInfo.IME_ACTION_SEARCH -> {
                     searchFeedingRooms(v.text.toString())
+                    hideKeyboard()
                     true
                 }
                 else -> false
             }
         }
+    }
+
+    private fun hideKeyboard() {
+        (context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)
+            ?.hideSoftInputFromWindow(vb.tvSearch.windowToken, 0)
     }
 
     private fun setupRecyclerView() {
