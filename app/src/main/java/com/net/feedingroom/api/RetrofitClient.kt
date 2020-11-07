4bdc7e5581c7c0bc2ec677f2bbc19ce6b2202f9d
Tony <tonywu@taiwanmobile.com>
2020-11-07 14:04:10 +0800

    set connectionTimeOut and readTimeout setting for retrofit

--- app/src/main/java/com/net/feedingroom/api/RetrofitClient.kt
+++ app/src/main/java/com/net/feedingroom/api/RetrofitClient.kt
@@ -1,12 +1,20 @@
 package com.net.feedingroom.api
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
+    private val okHttpClient =
+            OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+
     var client = Retrofit.Builder()
+                .client(okHttpClient.build())
                 .baseUrl("http://babyfeedingroom.tech-farmer.life")
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
