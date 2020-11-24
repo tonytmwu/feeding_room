294af5d5b395482706e1dd66694e92df576be749
Tony <tonywu@taiwanmobile.com>
2020-11-24 13:18:07 +0800

    update app version

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -4,7 +4,7 @@ object Config {
     const val compileSdk = targetSdk
     const val minSdk = 21
 
-    const val versionCode = 3
-    const val versionName = "1.2"
+    const val versionCode = 6
+    const val versionName = "1.5"
 
 }
