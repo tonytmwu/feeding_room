af9c9ca5d5af2c6142f3d07283c2907b07a18303
tony_wu <Koucha0904>
2022-08-10 10:27:42 +0800

    更新 google map key

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -4,7 +4,7 @@ object Config {
     const val compileSdk = "android-32"
     const val minSdk = 21
 
-    const val versionCode = 6
-    const val versionName = "1.5"
+    const val versionCode = 8
+    const val versionName = "1.7"
 
 }
